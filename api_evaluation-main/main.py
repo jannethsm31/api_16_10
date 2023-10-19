cff519dbf4570d4b98eec5686f8d87c6ff2a0574
janneth santos <1722110301@utectulancingo.edu.mx>
2023-10-19 15:33:26 +0000

    Commit

---
+++ api_evaluation-main/main.py
@@ -0,0 +1,72 @@
+import fastapi
+import sqlite3
+from pydantic import BaseModel
+
+# Crea la base de datos
+conn = sqlite3.connect("contactos.db")
+
+app = fastapi.FastAPI()
+
+class Contacto(BaseModel):
+    email: str
+    nombre: str
+    telefono: str
+
+# Rutas para las operaciones CRUD
+
+@app.post("/contactos")
+async def crear_contacto(contacto: Contacto):
+    """Crea un nuevo contacto."""
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+                    (contacto.email, contacto.nombre, contacto.telefono))
+    conn.commit()
+    return contacto
+
+@app.get("/contactos")
+async def obtener_contacto():
+    """Obtiene todos los contactos."""
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM contactos')
+    response = []
+    for row in cursor:
+        contacto = Contactos(email=row[0], nombre=row[1], telefono=row[2])
+        response.append(contacto)
+    return response
+
+@app.get("/contactos/{email}")
+async def obtener_contacto(email: str):
+    """Obtiene un contacto por su email."""
+    # Consulta el contacto por su email
+    coursor = conn.cursor()
+    cursor.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+    row = cursor.fetchone()
+    if row: 
+        contacto = Contacto(email=row[0], nombre=row[1], telefono=row[2])
+        return contacto
+    else:
+        return None
+
+@app.put("/contactos/{email}")
+async def actualizar_contacto(email: str, contacto: Contacto):
+    """Actualiza un contacto."""
+    # TODO Actualiza el contacto en la base de datos
+    cursor = conn.cursor()
+    cursor.execute('UPDATE contactos SER nombre = ?, telefono = ? WHERE email = ?',
+              (contacto.nombre, contacto.telefono, contacto.email))
+    conn.commit()
+    return contacto
+
+
+@app.delete("/contactos/{email}")
+async def eliminar_contacto(email: str):
+    """Elimina un contacto."""
+    # TODO Elimina el contacto de la base de datos
+    cursor = conn.cursor()
+    cursor.execute('DELETE contactos WHERE email = ?', (email,))
+    conn.commit()
+    return {"message": "Contacto eliminado con éxito"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
