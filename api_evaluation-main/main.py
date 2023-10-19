5c066e2519d8e380991f6c079b470cbd8613bd23
janneth santos <1722110301@utectulancingo.edu.mx>
2023-10-19 16:44:48 +0000

    Punto 5 resuelto

--- api_evaluation-main/main.py
+++ api_evaluation-main/main.py
@@ -44,15 +44,14 @@ async def obtener_contacto(email: str):
     if row in c: 
         contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]})
         return contacto
-    else:
-        return None
+
 
 @app.put("/contactos/{email}")
 async def actualizar_contacto(email: str, contacto: Contacto):
     """Actualiza un contacto."""
     # TODO Actualiza el contacto en la base de datos
-    cursor = conn.cursor()
-    cursor.execute('UPDATE contactos SER nombre = ?, telefono = ? WHERE email = ?',
+    c = conn.cursor()
+    c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
               (contacto.nombre, contacto.telefono, contacto.email))
     conn.commit()
     return contacto
